8fb63b1c169a7bf82e3fe862f9d4355a3a275d8d
Sebastián Magrí <sebasmagri@gmail.com>
2017-07-20 05:07:36 -0500

    Implement a dynamically server-side rendered script to display ads in browsers. Update README.

--- README.md
+++ README.md
@@ -11,3 +11,52 @@ The content of the ads could be:
 - Surveys
 
 Initially, we just want to serve simple campaigns. There will not be any tracking, targetting, scheduling, frequency management, custom sizing, categorization or reporting initially. All of this could be implemented in the future.
+
+## Usage
+
+### Adding campaigns
+
+Please create an issue in this repository or contact `community-team@rust-lang.org` with a title, start and end date, and a destination URL for your campaign. You can also add a brief description to it.
+
+### Displaying Rust Campaigns in your website
+
+There are several options to display Rust campaigns in a website.
+
+#### Using the provided script
+
+You can use the `campaigns.js` file located at the root of the _Rust Campaigns Server_ (TBD) and put it in the place where you want the campaigns to be rendered. It will render a randomized set of active campaigns.
+
+The script response will be cached for 6 hours by default. You can use a random cache bust parameter to override this. *This is not yet implemented*.
+
+The script can also get an `l=n` parameter to indicate the number of campaigns to load. For example, `https://<rcs-server-url>/campaigns.js?l=10` will load at most 10 randomized active campaigns.
+
+##### Styling campaigns with the provided script
+
+The provided script renders a `div` component for each campaign. This is the markup for such element:
+
+```
+<div class="rcs-campaign">
+    <h3><a href="CLICK-URL">TITLE</a><h3>
+    <div class="rcs-description">
+        DESCRIPTION
+    </div>
+</div>
+```
+
+#### Using the API
+
+There is a simple API available at `/api/v1/` you can use to fetch campaigns if you want to implement custom rendering. The `/api/v1/campaigns` endpoint will behave just as the provided script and will return a JSON array of randomized active campaigns.
+
+You can also use the `l` parameter to indicate the number of campaigns to load.
+
+#### Using the reference JavaScript client
+
+*Note: not implemented*
+
+The reference JavaScript client implements communication with the API so you don't have to. Then, you can use it to easily fetch the data and implement custom rendering of the campaigns.
+
+```
+var rcsClient = new RCSClient();
+var rustCampaigns = rcsClient.load(5);
+...
+```
--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,8 @@ use std::env;
 mod database {
     use super::{Pool,ConnectionManager,PgConnection};
 
-    pub type DBPool = Pool<ConnectionManager<PgConnection>>;    
+    pub type DBConnection = PgConnection;
+    pub type DBPool = Pool<ConnectionManager<DBConnection>>;
 }
 
 
@@ -45,6 +46,11 @@ mod schema {
 
 mod models {
     use super::chrono::NaiveDateTime;
+    use super::database::DBConnection;
+    use super::diesel::prelude::*;
+    use super::diesel::expression::dsl::now;
+    use super::rand::{thread_rng,Rng};
+    use super::schema::campaigns::dsl::{campaigns,start_date as start};
     use super::serde_json::Value;
 
     #[derive(Serialize)]
@@ -73,58 +79,98 @@ mod models {
         end_date: Option<NaiveDateTime>,
         click_url: String
     }
+
+    impl Campaign {
+        // returns a vector of `limit` random active campaigns
+        pub fn random_set(conn: &DBConnection, limit: usize) -> Vec<Campaign> {
+            let active_campaigns_qry = campaigns
+                .filter(start.lt(now))
+            //            .filter(end_date.gt(now))
+                .load::<Campaign>(conn);
+
+            match active_campaigns_qry {
+                Ok(mut active_campaigns) => {
+                    let camps: Vec<Campaign> = {
+                        // shuffle the campaigns
+                        let mut rng = thread_rng();
+                        rng.shuffle(&mut active_campaigns);
+
+                        if active_campaigns.len() > limit {
+                            // pick `limit` campaigns
+                            (active_campaigns[..limit]).to_vec()
+                        } else {
+                            active_campaigns
+                        }
+                    };
+
+                    camps
+                },
+                Err(_) => vec![]
+            }
+        }
+    }
 }
 
 
 mod handlers {
+
     use super::database::DBPool;
-    use super::diesel::prelude::*;
-    use super::diesel::expression::dsl::now;
     use super::models::{APIRoot,Campaign};
-    use super::rand::{thread_rng,Rng};
     use super::rocket::State;
-    use super::rocket_contrib::Json;
-    use super::schema::campaigns::dsl::*;
+    use super::rocket_contrib::{Json,Template};
+
+    #[derive(FromForm)]
+    struct CampaignsParams {
+        l: Option<usize>
+    }
+
+    const DEFAULT_LIMIT: usize = 5;
 
     // API endpoints
+
+    fn get_limit(pars: CampaignsParams) -> usize {
+        if let Some(l) = pars.l {
+            l
+        } else {
+            DEFAULT_LIMIT
+        }
+    }
 
     #[get("/campaigns")]
     fn get_campaigns(pool: State<DBPool>) -> Json<Vec<Campaign>> {
         let ref conn = *pool.clone().get().unwrap();
+        Json(Campaign::random_set(&conn, DEFAULT_LIMIT))
+    }
 
-        let active_campaigns_qry = campaigns
-            .filter(start_date.lt(now))
-//            .filter(end_date.gt(now))
-            .load::<Campaign>(conn);
-
-        let limit = 5;
-
-        match active_campaigns_qry {
-            Ok(mut active_campaigns) => {
-                let camps: Vec<Campaign> = {
-                    // shuffle the campaigns
-                    let mut rng = thread_rng();
-                    rng.shuffle(&mut active_campaigns);
-
-                    if active_campaigns.len() > limit {
-                        // pick `limit` campaigns
-                        (active_campaigns[..limit]).to_vec()
-                    } else {
-                        active_campaigns
-                    }
-                };
-
-                Json(
-                    camps
-                )
-            },
-            Err(_) => Json(vec![])
-        }
+    #[get("/campaigns?<pars>", rank = 2)]
+    fn get_campaigns_with_pars(pool: State<DBPool>, pars: CampaignsParams) -> Json<Vec<Campaign>> {
+        let ref conn = *pool.clone().get().unwrap();
+        Json(Campaign::random_set(&conn, get_limit(pars)))
     }
 
     #[get("/")]
     fn api_root<'a>() -> Json<APIRoot<'a>> {
         Json(APIRoot::new())
+    }
+
+    #[get("/campaigns.js", format = "application/javascript")]
+    fn campaigns_script(pool: State<DBPool>) -> Template {
+        let ref conn = *pool.clone().get().unwrap();
+
+        let context = json!({
+            "campaigns": Campaign::random_set(conn, DEFAULT_LIMIT)
+        });
+        Template::render("script", &context)
+    }
+
+    #[get("/campaigns.js?<pars>", format = "application/javascript")]
+    fn campaigns_script_with_pars(pool: State<DBPool>, pars: CampaignsParams) -> Template {
+        let ref conn = *pool.clone().get().unwrap();
+
+        let context = json!({
+            "campaigns": Campaign::random_set(conn, get_limit(pars))
+        });
+        Template::render("script", &context)
     }
 }
 
@@ -148,7 +194,11 @@ fn main() {
 
     rocket::ignite()
         .manage(pool)
+        .attach(rocket_contrib::Template::fairing())
         .mount("/api/v1/", routes![handlers::api_root,
-                            handlers::get_campaigns])
+                                   handlers::get_campaigns,
+                                   handlers::get_campaigns_with_pars])
+        .mount("", routes![handlers::campaigns_script,
+                           handlers::campaigns_script_with_pars])
         .launch();
 }
